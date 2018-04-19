ed128d97f640ccd85ff11a8a65f71e207dd9cf26
AtomicInteger <rostyk1@hotmail.com>
2018-04-20 01:21:17 +0300

    Improved Stack's tests

--- src/stack/stack.rs
+++ src/stack/stack.rs
@@ -1,5 +1,3 @@
-extern crate core;
-
 use vector_based_dstruct::VectorBasedDataStructure;
 
 pub struct Stack<T> {
@@ -46,5 +44,71 @@ impl<T> VectorBasedDataStructure<T> for Stack<T> {
 
     fn new() -> Stack<T> {
         Stack { entry: Vec::new() }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Stack;
+    use vector_based_dstruct::VectorBasedDataStructure;
+
+    #[test]
+    fn push() {
+        let mut stack = Stack::new();
+        assert!(stack.is_empty());
+        stack.push(0);
+        assert!(!stack.is_empty());
+        assert_eq!(stack.size(), 1);
+        stack.push(1);
+        assert_eq!(stack.size(), 2);
+        stack.push(2);
+        assert_eq!(stack.size(), 3);
+    }
+
+    #[test]
+    fn pop() {
+        let mut stack = Stack::from(vec![1, 2, 3, 4, 5, 6, 7, 8, 9]);
+        assert_eq!(stack.size(), 9);
+        let last = stack.pop().unwrap();
+        assert_eq!(stack.size(), 8);
+        assert_eq!(last, 9);
+        let last = stack.pop().unwrap();
+        assert_eq!(stack.size(), 7);
+        assert_eq!(last, 8);
+        let mut empty_stack: Stack<i32> = Stack::new();
+        assert!(empty_stack.is_empty());
+        let last = empty_stack.pop();
+        assert_eq!(last, None);
+    }
+
+    #[test]
+    fn peek() {
+        let stack = Stack::from(vec![1, 2, 3]);
+        assert_eq!(stack.size(), 3);
+        let last = stack.peek().unwrap();
+        assert_eq!(stack.size(), 3);
+        assert_eq!(last, &3);
+        let empty_stack : Stack<i32> = Stack::new();
+        assert_eq!(empty_stack.size(), 0);
+        let last = empty_stack.peek();
+        assert_eq!(last, None);
+    }
+
+    #[test]
+    fn clear() {
+        let mut stack = Stack::from(vec![1, 2, 3]);
+        assert!(!stack.is_empty());
+        stack.clear();
+        assert!(stack.is_empty());
+    }
+
+    #[test]
+    fn is_empty() {
+        let stack = Stack::from(vec![1, 2, 3]);
+        assert!(!stack.is_empty());
+        assert_ne!(stack.size(), 0);
+        let empty_stack: Stack<i32> = Stack::new();
+        assert!(empty_stack.is_empty());
+        assert_eq!(empty_stack.size(), 0);
     }
 }
--- tests/stack/stack_test.rs
+++ tests/stack/stack_test.rs
@@ -4,48 +4,7 @@ use self::dstruct::stack::stack::Stack;
 use self::dstruct::vector_based_dstruct::VectorBasedDataStructure;
 
 #[test]
-fn push_stack() {
-    let mut stack = Stack::new();
-    stack.push(1);
-    stack.push(2);
-    stack.push(3);
-    assert_eq!(stack.size(), 3);
-}
-
-#[test]
-fn peek_stack() {
-    let stack = Stack::from(vec![1, 2, 5]);
-    assert_eq!(stack.peek().unwrap().to_owned(), 5);
-    assert_eq!(stack.size(), 3);
-}
-
-#[test]
-fn clear_stack() {
-    let mut not_empty_stack = Stack::from(vec![1, 2, 5]);
-    assert_eq!(not_empty_stack.is_empty(), false);
-    not_empty_stack.clear();
-    assert_eq!(not_empty_stack.is_empty(), true);
-}
-
-#[test]
-fn is_empty_stack() {
-    let not_empty_stack = Stack::from(vec![1, 2, 5]);
-    assert_eq!(not_empty_stack.is_empty(), false);
-    assert_ne!(not_empty_stack.size(), 0);
-    let empty_stack: Stack<i32> = Stack::new();
-    assert_eq!(empty_stack.is_empty(), true);
-    assert_eq!(empty_stack.size(), 0);
-}
-
-#[test]
-fn pop_stack() {
-    let mut stack = Stack::from(vec![1, 2, 3]);
-    assert_eq!(stack.pop().unwrap(), 3);
-    assert_eq!(stack.size(), 2);
-}
-
-#[test]
-fn push_and_pop_stack() {
+fn push_and_pop() {
     let mut stack = Stack::new();
     stack.push(1);
     assert_eq!(stack.size(), 1);
@@ -54,5 +13,70 @@ fn push_and_pop_stack() {
     assert_eq!(stack.pop().unwrap(), 2);
     assert_eq!(stack.size(), 1);
     assert_eq!(stack.pop().unwrap(), 1);
+    assert_eq!(stack.size(), 0);
+    assert_eq!(stack.pop(), None);
+    assert_eq!(stack.size(), 0);
+}
+
+#[test]
+fn push_and_peek() {
+    let mut stack = Stack::new();
+    stack.push(1);
+    assert_eq!(stack.size(), 1);
+    assert_eq!(stack.peek().unwrap(), &1);
+    stack.push(2);
+    assert_eq!(stack.size(), 2);
+    assert_eq!(stack.peek().unwrap(), &2);
+    assert_eq!(stack.size(), 2);
+}
+
+#[test]
+fn push_and_clear() {
+    let mut stack = Stack::new();
+    stack.push(1);
+    assert_eq!(stack.size(), 1);
+    stack.push(2);
+    assert_eq!(stack.size(), 2);
+    stack.clear();
+    assert!(stack.is_empty());
+    stack.push(3);
+    stack.push(4);
+    stack.push(5);
+    assert_eq!(stack.size(), 3);
+    stack.clear();
+    assert!(stack.is_empty());
+}
+
+#[test]
+fn peek_and_pop() {
+    let mut stack = Stack::from(vec![1, 2, 3, 4, 5, 6]);
+    assert_eq!(stack.size(), 6);
+    assert_eq!(stack.peek().unwrap(), &6);
+    assert_eq!(stack.size(), 6);
+    assert_eq!(stack.pop().unwrap(), 6);
+    assert_eq!(stack.size(), 5);
+    assert_eq!(stack.peek().unwrap(), &5);
+    assert_eq!(stack.size(), 5);
+    assert_eq!(stack.pop().unwrap(), 5);
+    assert_eq!(stack.size(), 4);
+    assert_eq!(stack.peek().unwrap(), &4);
+    assert_eq!(stack.size(), 4);
+    assert_eq!(stack.pop().unwrap(), 4);
+    assert_eq!(stack.size(), 3);
+    assert_eq!(stack.peek().unwrap(), &3);
+    assert_eq!(stack.size(), 3);
+    assert_eq!(stack.pop().unwrap(), 3);
+    assert_eq!(stack.size(), 2);
+    assert_eq!(stack.peek().unwrap(), &2);
+    assert_eq!(stack.size(), 2);
+    assert_eq!(stack.pop().unwrap(), 2);
+    assert_eq!(stack.size(), 1);
+    assert_eq!(stack.peek().unwrap(), &1);
+    assert_eq!(stack.size(), 1);
+    assert_eq!(stack.pop().unwrap(), 1);
+    assert_eq!(stack.size(), 0);
+    assert_eq!(stack.peek(), None);
+    assert_eq!(stack.size(), 0);
+    assert_eq!(stack.pop(), None);
     assert_eq!(stack.size(), 0);
 }
